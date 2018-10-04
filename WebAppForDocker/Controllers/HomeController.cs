9a159edbaefbcc9d28a501d6c0926df7fcdded39
jeppzone <jeppzone@gmail.com>
2018-10-04 09:40:58 +0200

    First version of the webapp

---
+++ WebAppForDocker/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using WebAppForDocker.Models;
+
+namespace WebAppForDocker.Controllers
+{
+    public class HomeController : Controller
+    {
+        readonly IOptions<AppSettings> _appSettings;
+
+        public HomeController(IOptions<AppSettings> appSettings ) {
+            _appSettings = appSettings;
+        }
+
+        public IActionResult Index() {
+            ViewData["Environment"] = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+            return View(_appSettings.Value);
+        }
+
+    }
+}
---
+++ WebAppForDocker/Models/AppSettings.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace WebAppForDocker.Models
+{
+    public class AppSettings
+    {
+        public String BackgroundColor { get; set; }
+        public String Text { get; set; }
+
+        public AppSettings(){}
+    }
+}
